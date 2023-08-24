0f2ae6f570230efeeefe423e8af7609a708c95cd
marilin <puchetamarilin@gmail.com>
2023-08-24 02:40:32 -0300

    Contacto

--- campusafa/src/components/Contacto/Contacto.jsx
+++ campusafa/src/components/Contacto/Contacto.jsx
@@ -1,31 +1,42 @@
 
 
 
+export function Contacto() {
+  return (
+    <>
+      <main class="main-contacto">
+        <section class="contenedor-contacto">
+          <div class="contacto">
+            <h1>CONTACTO</h1>
+          </div>
+        </section>
+        <section class="contacto-info">
+          <h2>PARA MÁS INFORMACIÓN</h2>
+          <br></br>
+          <form class="cont-form" id="formContactos">
+            <label for="nombreyapellido"></label>
+            <input class="control" id="nombreyapellido" type="text" placeholder="Nombre y Apellido:" required></input>
+            <label for="email"></label>
+            <input class="control" id="email" type="email" placeholder="Email:" required></input>
+            <div class="asunto">
+              <select class="asunt-cont" name="asunto" id="asunto">
+                <option selected disabled>ASUNTO</option>
+                <option value="jugador">Jugador</option>
+                <option value="cuerpotecnico">Técnico</option>
+                <option value="directivo">Directivo</option>
+                <option value="spondor">Sponsor</option>
+              </select>
+            </div>
+            <div class="comentario">
+              <textarea class="comentario-tex" id="comentario" rows="10" cols="20" placeholder="Comentario:"></textarea>
+            </div>
+            <div class="boton">
+              <button type="submit" id="enviar-cont">ENVIAR</button>
+            </div>
+          </form>
+        </section>
+      </main>
 
-<section class="contenedor-contacto">
-      <div class="contacto"><h1>CONTACTO</h1></div>
-    <section class="contacto-info">
-      <h2>PARA MÁS INFORMACIÓN</h2>
-      <br>
-      <form class="cont-form" id="formContactos">
-        <label for="nombreyapellido"></label>
-        <input class="control" id ="nombreyapellido" type="text"  placeholder="Nombre y Apellido:" required>
-        <label for="email"></label>
-        <input class="control" id ="email" type="email" placeholder="Email:" required>
-          <div class="asunto"> 
-            <select class="asunt-cont" name="asunto" id="asunto">
-              <option selected disabled>ASUNTO</option>
-              <option value="jugador">Jugador</option>
-              <option value="cuerpotecnico">Técnico</option>
-              <option value="directivo">Directivo</option>
-              <option value="spondor">Sponsor</label> 
-            </select>
-          </div>
-          <div class="comentario">
-            <textarea  class="comentario-tex" id="comentario" rows="10" cols="20" placeholder="Comentario:"></textarea>
-          </div>
-        <div class="boton">
-          <button type="submit" id="enviar-cont">ENVIAR</button>
-        </div>
-      </form>
-    </section>
+    </>
+  );
+}
