8c2d289ec4d2ebdaab82798e036497d0140a9c13
marilin <puchetamarilin@gmail.com>
2023-08-27 01:37:31 -0300

    Form
     formulario de contacto./logo de afa.

--- campusafa/src/components/Contacto/Contacto.jsx
+++ campusafa/src/components/Contacto/Contacto.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-// import { useState } from "react";
+import { useState } from "react";
 import './Contacto.css';
 
 
 export function Contacto() {
-  // const [formulario, setformulario] = useState({});
+  const [formulario, setFormulario] = useState({ nombreyapellido: "", email: "", asunto:"", comentario:""});
   return (
     <>
       <main className="main-contacto">
@@ -16,13 +16,13 @@ export function Contacto() {
         <section className="contacto-info">
           <h2>PARA MÁS INFORMACIÓN</h2>
           <br></br>
-          <form className="cont-form" id="formContactos">
+          <form onSubmit={() => alert(JSON.stringify(formulario))} className="cont-form" id="formContactos">
               <label for="nombreyapellido"></label>
-              <input className="control" id="nombreyapellido" type="text" placeholder="Nombre y Apellido:" required></input>
+              <input className="control" id="nombreyapellido" type="text" placeholder="Nombre y Apellido:"value={formulario.nombreyapellido}  required onChange={(e) => setFormulario({ ...formulario, "nombreyapellido": e.target.value })} ></input>
               <label for="email"></label>
-              <input className="control" id="email" type="email" placeholder="Email:" required></input>
+              <input className="control" id="email" value={formulario.email} type="email" placeholder="Email:" required onChange={(e) => setFormulario({ ...formulario, "email": e.target.value })}></input>
               <div className="asunto">
-                <select className="asunt-cont" name="asunto" id="asunto">
+                <select className="asunt-cont" name="asunto" value={formulario.asunto} id="asunto" onChange={(e) => setFormulario({ ...formulario, "asunto": e.target.value })}>
                   <option selected disabled>ASUNTO</option>
                   <option value="jugador">Jugador</option>
                   <option value="cuerpotecnico">Técnico</option>
@@ -31,7 +31,7 @@ export function Contacto() {
                 </select>
               </div>
               <div className="comentario">
-                <textarea className="comentario-tex" id="comentario" rows="10" cols="20" placeholder="Comentario:"></textarea>
+                <textarea className="comentario-tex" id="comentario" value={formulario.comentario} rows="10" cols="20" placeholder="Comentario:" onChange={(e) => setFormulario({ ...formulario, "comentario": e.target.value })}></textarea>
               </div>
               <div className="boton">
                 <button type="submit" id="enviar-cont">ENVIAR</button>
--- campusafa/src/components/Header/Header.jsx
+++ campusafa/src/components/Header/Header.jsx
@@ -1,17 +1,16 @@
- 
+
 import React from 'react';
 import './Header.css';
-import {Link}  from 'react-router-dom';
+import { Link } from 'react-router-dom';
 export function Header() {
-    const Logo = <img src='/public/Logo_AFA-02.png'  alt="Logo de la AFA"/>
+    const Logo = './logo_afa.png';
+    const start = <img alt="logo" src={Logo} width="45px" />;
     return (
         <>
-            <div className="logoNav">
-                <Link to="/"> {Logo}
-                </Link> 
-            </div>
+
             <nav>
                 <ul className="menu">
+                    <li className="logoNav"><Link to="/"> {start}</Link></li>
                     <li><Link to="institucional">INSTITUCIONAL</Link></li>
                     <li><Link to="contacto">CONTACTO</Link></li>
                 </ul>
