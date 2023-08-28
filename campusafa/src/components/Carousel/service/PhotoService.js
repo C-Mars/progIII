7ea7f938bc80f05166804b0e35694c5fe990d05d
marilin <puchetamarilin@gmail.com>
2023-08-28 18:23:21 -0300

    Institucional
    Galería primer-ract

--- campusafa/src/components/Carousel/service/PhotoService.js
+++ campusafa/src/components/Carousel/service/PhotoService.js
@@ -2,34 +2,34 @@ export const PhotoService = {
     getData() {
         return [
             {
-                itemImageSrc: '../galeria/5.jpg',
-                thumbnailImageSrc: '../galeria/5.jpg',
-                alt: 'Description for Image 1',
-                title: 'Title 1'
-            },
-            {
                 itemImageSrc: '../galeria/1.jpg',
                 thumbnailImageSrc: '../galeria/1.jpg',
+                alt: '',
+                title: '1'
+            },
+            {
+                itemImageSrc: '../galeria/5.jpg',
+                thumbnailImageSrc: '../galeria/5.jpg',
                 alt: 'Description for Image 2',
-                title: 'Title 2'
+                title: '2'
             },
             {
                 itemImageSrc: '../galeria/2.jpg',
                 thumbnailImageSrc: '../galeria/2.jpg',
                 alt: 'Description for Image 3',
-                title: 'Title 3'
+                title: '3'
             },
             {
                 itemImageSrc: '../galeria/3.jpg',
                 thumbnailImageSrc: '../galeria/3.jpg',
                 alt: 'Description for Image 4',
-                title: 'Title 4'
+                title: '4'
             },
             {
                 itemImageSrc: '../galeria/4.jpg',
                 thumbnailImageSrc: '../galeria/4.jpg',
                 alt: 'Description for Image 5',
-                title: 'Title 5'
+                title: '5'
             }
         ];
     },
--- campusafa/src/components/Institucional/Institucional.jsx
+++ campusafa/src/components/Institucional/Institucional.jsx
@@ -1,33 +1,7 @@
 import './Institucional.css'
-import '../../primereact-theme/theme.css'
-import React, { useState, useEffect } from 'react';
-import { Galleria } from 'primereact/galleria';
-import { PhotoService } from './service/PhotoService';
-
-export default function ItemWithoutThumbnailsDemo() {
-    const [images, setImages] = useState(null);
-
-    useEffect(() => {
-            PhotoService.getImages().then(data => setImages(data));
-    }, []);
-
-    const itemTemplate = (item) => {
-        return <img src={item.itemImageSrc} alt={item.alt} style={{ width: '100%', display: 'block' }} />;
-    }
-
-    const thumbnailTemplate = (item) => {
-        return <img src={item.thumbnailImageSrc} alt={item.alt} style={{ display: 'block' }} />;
-    }
-
-    return (
-        <div className="card"> 
-            <Galleria value={images} numVisible={5} circular style={{ maxWidth: '640px' }}
-                showThumbnails={false} showItemNavigators item={itemTemplate} thumbnail={thumbnailTemplate} />
-        </div>
-    )
-}
-
-
+import '../../primereact-theme/theme.css';
+import "primereact/resources/primereact.min.css";
+import Carousel from '../Carousel/Carousel';
 
 export function Institucional() {
     return (
@@ -42,7 +16,7 @@ export function Institucional() {
 
                     </div>
                     <div className='galeria-int'>
-                        <ItemWithoutThumbnailsDemo/>
+                        <Carousel/>
                     </div>
                 </section >
             </main >
